3235b427e5d7c62eec6ae0aa7d199ab8695c6a20
LukasZvikas <lzvikas1@gmail.com>
2018-06-06 22:05:50 -0500

    added authentication boilerplate

---
+++ src/components/screens/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Home extends Component {
+  render() {
+    return <View>This is Home component</View>;
+  }
+}
+
+export default Home;
